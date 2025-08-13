cae56ccfa68619f26c3b96286a2dd172ac6d7817
Zayn Hamid <91781657+ZaynHamid@users.noreply.github.com>
2025-08-13 21:51:56 +0500

    Update schema.js

--- schema.js
+++ schema.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const uri = process.env.MONGODB_URI;
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(uri)
 .then(() => console.log("Connected to MongoDB Atlas"))
 .catch(err => console.error("MongoDB connection error:", err));
 
